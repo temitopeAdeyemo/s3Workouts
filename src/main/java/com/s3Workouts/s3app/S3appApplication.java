dc8b3b94ece9ea39ea3d2923fc97f15364e301ae
temitopeAdeyemo <temitopejulius9@gmail.com>
2024-10-06 07:03:06 +0100

    Initialised project

---
+++ src/main/java/com/s3Workouts/s3app/S3appApplication.java
@@ -0,0 +1,13 @@
+package com.s3Workouts.s3app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s3Workouts/s3app/S3appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3Workouts.s3app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
