0ecb33424db9e19e45572d778b1fdbd68d9cd780
temitopeAdeyemo <temitopejulius9@gmail.com>
2024-10-09 11:41:03 +0100

    Implemented cloudfront url signing policies

--- src/main/java/com/s3Workouts/s3app/FileProcessor.java
+++ src/main/java/com/s3Workouts/s3app/FileProcessor.java
@@ -11,12 +11,22 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudfront.CloudFrontUtilities;
+import software.amazon.awssdk.services.cloudfront.model.CannedSignerRequest;
+import software.amazon.awssdk.services.cloudfront.model.CustomSignerRequest;
+import software.amazon.awssdk.services.cloudfront.url.SignedUrl;
 import software.amazon.awssdk.services.s3.*;
 import software.amazon.awssdk.services.s3.model.*;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.ByteBuffer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Time;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -24,6 +34,15 @@ import java.util.UUID;
 @RestController
 @RequestMapping
 public class FileProcessor {
+    private static final CloudFrontUtilities cloudFrontUtilities =
+            CloudFrontUtilities.create();
+
+    @Value("${aws.cloudfront-ddn}")
+    private String CLOUD_FRONT_DISTRIBUTION_DOMAIN_NAME;
+
+    @Value("${aws.cloudfront-public-key}")
+    private String CLOUD_FRONT_PUBLIC_KEY;
+
     @Value("${aws.bucket-name}")
     private String bucketName;
 
@@ -35,7 +54,7 @@ public class FileProcessor {
                 .profileName("default")
                 .build();
 
-        Region region = Region.EU_NORTH_1;
+        Region region = Region.US_EAST_1;
         S3Client s3Client = S3Client.builder()
                 .region(region)
                 .credentialsProvider(credentialsProvider)
@@ -102,21 +121,20 @@ public class FileProcessor {
     }
 
     @PostMapping("/a")
-    public ResponseEntity<Object> init2(@RequestParam("file") MultipartFile filed) {
+    public ResponseEntity<Object> init2(@RequestParam("file") MultipartFile filed) throws Exception {
 
         ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.builder()
                 .profileName("default")
                 .build();
 
-        Region region = Region.EU_NORTH_1;
+        Region region = Region.US_EAST_1;
 
         S3Client s3Client = S3Client.builder()
                 .region(region)
                 .credentialsProvider(credentialsProvider)
                 .build();
-        var time = Time.from(Instant.now()).toString();
 
-        String key = filed.getName()+ "-"+UUID.randomUUID() + "-" + time;
+        String key = filed.getName()+ "-"+UUID.randomUUID(); //+ "-" + time;
 
         try {
             byte[] bytes = filed.getBytes();
@@ -134,6 +152,78 @@ public class FileProcessor {
 
         System.out.println("Uploaded object URL: " + objectUrl);
 
-        return new ResponseEntity<>("Multipart file upload completed successfully! "+ objectUrl, HttpStatus.OK);
+        Path privateKeyPath = Paths.get(".aws/cloudfront_private_key.pem");
+//        var cannedSignerRequestCanned = createRequestForCannedPolicy(CLOUD_FRONT_DISTRIBUTION_DOMAIN_NAME, key, privateKeyPath.toAbsolutePath().toString(), CLOUD_FRONT_PUBLIC_KEY);
+
+        var cannedSignerRequestCustom = createRequestForCustomPolicy(CLOUD_FRONT_DISTRIBUTION_DOMAIN_NAME, key, privateKeyPath.toAbsolutePath().toString(), CLOUD_FRONT_PUBLIC_KEY);
+
+//        var signedUrlForCannedPolicy = signUrlForCannedPolicy(cannedSignerRequestCanned);
+        var signedUrlForCustomPolicy = signUrlForCustomPolicy(cannedSignerRequestCustom);
+
+        return new ResponseEntity<>(signedUrlForCustomPolicy.url() + " Multipart file upload completed successfully! "+ objectUrl, HttpStatus.OK);
+    }
+
+    public static CannedSignerRequest createRequestForCannedPolicy(String distributionDomainName,
+                                                                   String fileNameToUpload,
+                                                                   String privateKeyFullPath,
+                                                                   String publicKeyId
+    ) throws Exception {
+        String protocol = "https";
+        String resourcePath = "/" + fileNameToUpload;
+        System.out.println("** "+ protocol + " " + distributionDomainName +  " " +resourcePath);
+        String cloudFrontUrl = new URL(protocol, distributionDomainName, resourcePath).toString();
+        Instant expirationDate = Instant.now().plus(1, ChronoUnit.MINUTES);
+        Path path = Paths.get(privateKeyFullPath);
+
+        return CannedSignerRequest.builder()
+                .resourceUrl(cloudFrontUrl)
+                .privateKey(path)
+                .keyPairId(publicKeyId)
+                .expirationDate(expirationDate)
+                .build();
+    }
+
+    public static CustomSignerRequest createRequestForCustomPolicy(String
+                                                                           distributionDomainName,
+                                                                   String fileNameToUpload,
+                                                                   String privateKeyFullPath, String publicKeyId) throws Exception {
+        String protocol = "https";
+        String resourcePath = "/" + fileNameToUpload;
+        String cloudFrontUrl = new URL(protocol, distributionDomainName,
+                resourcePath).toString();
+//        Instant expireDate = Instant.now().plus(2, ChronoUnit.MINUTES);
+//        // URL will be accessible tomorrow using the signed URL.
+//        Instant activeDate = Instant.now().plus(1, ChronoUnit.MINUTES);
+
+        // Set the Lagos time zone
+        ZoneId lagosTimeZone = ZoneId.of("Africa/Lagos");
+
+        // Set expiration and active dates
+        ZonedDateTime lagosNow = ZonedDateTime.now(lagosTimeZone);
+        Instant expireDate = lagosNow.plusHours(5).toInstant();
+        Instant activeDate = lagosNow.plusHours(4).toInstant(); // Optional
+
+        System.out.println(expireDate + " " + activeDate);
+
+        Path path = Paths.get(privateKeyFullPath);
+        return CustomSignerRequest.builder()
+                .resourceUrl(cloudFrontUrl)
+                .privateKey(path)
+                .keyPairId(publicKeyId)
+                .expirationDate(expireDate)
+                .activeDate(activeDate) // Optional.
+                // .ipRange("192.168.0.1/24") // Optional.
+                .build();
+    }
+
+    public static SignedUrl signUrlForCannedPolicy(CannedSignerRequest
+                                                           cannedSignerRequest) {
+        //        logger.info("Signed URL: [{}]", signedUrl.url());
+        return cloudFrontUtilities.getSignedUrlWithCannedPolicy(cannedSignerRequest);
+    }
+
+    public static SignedUrl signUrlForCustomPolicy(CustomSignerRequest
+                                                           customSignerRequest) {
+        return cloudFrontUtilities.getSignedUrlWithCustomPolicy(customSignerRequest);
     }
 }
